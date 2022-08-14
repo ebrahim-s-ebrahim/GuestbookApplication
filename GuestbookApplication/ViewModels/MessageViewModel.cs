ed46d88672d8d681412e527cadfbf83535111339
ebrahim-s-ebrahim <ebrahim3adel@gmail.com>
2022-08-14 21:24:57 +0200

    Adding view models to control what should be returned to end user

---
+++ GuestbookApplication/ViewModels/MessageViewModel.cs
@@ -0,0 +1,10 @@
+﻿using GuestbookApplication.Models;
+
+namespace GuestbookApplication.ViewModels
+{
+    public class MessageViewModel
+    {
+        public int MessageId { get; set; }
+        public string MessageContent { get; set; }
+    }
+}
