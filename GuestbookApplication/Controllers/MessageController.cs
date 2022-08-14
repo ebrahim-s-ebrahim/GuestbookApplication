cf9a9353386a59dfe2d6cb975e5e5319d679d7d1
ebrahim-s-ebrahim <ebrahim3adel@gmail.com>
2022-08-14 17:33:18 +0200

    MessageController created

--- GuestbookApplication/Controllers/MessageController.cs
+++ GuestbookApplication/Controllers/MessageController.cs
@@ -24,5 +24,46 @@ namespace GuestbookApplication.Controllers
             var messages = await connection.QueryAsync<Messages>("select * from messages");
             return Ok(messages);
         }
+
+        [HttpGet("messageId")]
+        public async Task<ActionResult<Messages>> GetMessage(int messageId)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("Default"));
+            var message = await connection.QueryFirstAsync<Messages>("select * from messages where messageId = @Id", new { Id = messageId });
+            return Ok(message);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Messages>>> CreateMessage(Messages messages)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("Default"));
+            await connection.ExecuteAsync("insert into messages (messageContent) values (@MessageContent)", messages);
+            return Ok(await SelectAllMessages(connection));
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Messages>>> UpdateMessage(Messages messages)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("Default"));
+            await connection.ExecuteAsync("update messages set messageContent = @MessageContent where messageId = @Id", messages);
+            return Ok(await SelectAllMessages(connection));
+        }
+
+        [HttpDelete("{messageId}")]
+        public async Task<ActionResult<List<Messages>>> DeleteMessage(int messageId)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("Default"));
+            await connection.ExecuteAsync("delete from messages where messageId = @Id", new { Id = messageId });
+            return Ok(await SelectAllMessages(connection));
+        }
+
+
+
+
+
+        private static async Task<IEnumerable<Users>> SelectAllMessages(SqlConnection connection)
+        {
+            return await connection.QueryAsync<Users>("select * from messages");
+        }
     }
 }
