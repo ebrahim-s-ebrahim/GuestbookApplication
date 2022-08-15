a162edee1da72ceb61b8d516314edec74d8631bb
ebrahim-s-ebrahim <ebrahim3adel@gmail.com>
2022-08-15 16:34:45 +0200

    Fluent validation applied

---
+++ GuestbookApplication/Validators/MessageValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using GuestbookApplication.Models;
+
+namespace GuestbookApplication.Validators
+{
+    public class MessageValidator : AbstractValidator<Message>
+    {
+        public MessageValidator()
+        {
+            RuleFor(message => message.MessageId).NotNull();
+            RuleFor(message => message.MessageContent).NotEmpty();
+        }
+    }
+}
---
+++ GuestbookApplication/Validators/UserValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using GuestbookApplication.Models;
+
+namespace GuestbookApplication.Validators
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(user => user.UserName).NotEmpty();
+            RuleFor(user => user.Password).NotEmpty().MinimumLength(3);
+
+        }
+    }
+}
