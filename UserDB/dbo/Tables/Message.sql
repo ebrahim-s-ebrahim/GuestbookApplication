716b815a799ce22f42c0260b9556bad82fcdb852
ebrahim-s-ebrahim <ebrahim3adel@gmail.com>
2022-08-30 20:51:24 +0200

    sql server project added

---
+++ UserDB/dbo/Tables/Message.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Message]
+(
+	[messageId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [messageContent] VARCHAR(MAX) NULL, 
+    [userId] INT NULL, 
+    [parentMessageId] INT NULL
+)
---
+++ UserDB/dbo/Tables/User.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[userId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [username] NVARCHAR(50) NULL, 
+    [password] NVARCHAR(50) NULL
+)
